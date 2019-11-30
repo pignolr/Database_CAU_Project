de810f05722f955c0bbb845b05600bc532d94cf8
Rémi <remi.pignol@epitech.eu>
2019-11-30 17:41:53 +0900

    add registration and login

--- App/app/routes/auth/login.js
+++ App/app/routes/auth/login.js
@@ -1,16 +1,38 @@
 import { Router } from 'express'
 
+let error_message = null
+
 module.exports = Router({mergeParams: true})
     .get('/auth/login', async(req, res, next) => {
         var username = req.cookies.username;
 
         res.render('auth/login.ejs',  {
-            'username': username
+            'username': username,
+            'error_message': error_message
         });
+        error_message = null
         next();
     })
     .post('/auth/login', async(req, res, next) => {
-        res.cookie('username', req.body.username, { maxAge: 900000, httpOnly: true });
-        res.redirect('/');
+        let username = req.body.username
+
+        let response
+        try {
+            let querystring = "SELECT * FROM Customer WHERE username = :username LIMIT 1"
+            let params = {
+                "username": username
+            }
+            response = await req.db.query(querystring, params)
+        } catch (error) {
+            response = undefined
+            console.log('throw: ' + error)
+        }
+        if (response === undefined || !response.length) {
+            error_message = "This username doesn't exist"
+            res.redirect('/auth/login');
+        } else {
+            res.cookie('username', username, { maxAge: 900000, httpOnly: true });
+            res.redirect('/');    
+        }
         next();
     })
--- App/app/routes/auth/register.js
+++ App/app/routes/auth/register.js
@@ -7,12 +7,47 @@ module.exports = Router({mergeParams: true})
         var username = req.cookies.username;
 
         res.render('auth/register.ejs',  {
-            'username': username
+            'username': username,
+            'error_message': error_message
         });
+        error_message = null
         next();
     })
     .post('/auth/register', async(req, res, next) => {
-        res.cookie('username', req.body.username, { maxAge: 900000, httpOnly: true });
-        res.redirect('/');
+        let username = req.body.username
+
+        let response
+        try {
+            let querystring = "SELECT * FROM Customer WHERE username = :username LIMIT 1"
+            let params = {
+                "username": username
+            }
+            response = await req.db.query(querystring, params)
+        } catch (error) {
+            response = undefined
+            console.log('throw: ' + error)
+        }
+        if (response !== undefined && response.length > 0) {
+            error_message = "This username already exist"
+            res.redirect('/auth/register');
+        } else {
+            try {
+                let querystring = "INSERT INTO Customer (username) VALUES (:username);" 
+                let params = {
+                    "username": username
+                }
+                response = await req.db.query(querystring, params)
+            } catch (error) {
+                response = undefined
+                console.log('throw: ' + error)
+            }
+            if (response === undefined) {
+                error_message = "We can't add this username"
+                res.redirect('/auth/register');
+            } else {
+                res.cookie('username', req.body.username, { maxAge: 900000, httpOnly: true });
+                res.redirect('/');    
+            }
+        }
         next();
     })
